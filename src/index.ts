cf79b610ebbca76820621dbf523191e1de436295
Julien Dubocage <dubocage.julien68@gmail.com>
2023-10-24 14:33:31 +0200

    fix: migrations config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Hono } from 'hono'
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { drizzle, BunSQLiteDatabase } from 'drizzle-orm/bun-sqlite';
+import { Database } from 'bun:sqlite';
 
 const sqlite: Database = new Database('database.sqlite');
-const db: BetterSQLite3Database = drizzle(sqlite);
-await migrate(db, { migrationsFolder: "migrations" });
+const db: BunSQLiteDatabase = drizzle(sqlite);
+migrate(db, { migrationsFolder: "migrations" });
 
 const app = new Hono()
 
