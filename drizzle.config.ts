f9002b2a0d0f76b25332ac425bd1b1d6f2a482e9
Julien Dubocage <dubocage.julien68@gmail.com>
2023-11-08 16:36:52 +0100

    fix: clean unused medical act db schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit';
 
 export default {
-    schema: "src/db/schema/*.ts",
+    schema: 'src/db/schemas/*.ts',
     out: 'migrations',
     driver: 'better-sqlite',
     dbCredentials: {
         url: 'database.sqlite',
-    }
+    },
 } satisfies Config;
