7e6f7fc2ffdf3409a38275e12428232a5fcfec1a
Julien Dubocage <dubocage.julien68@gmail.com>
2023-11-10 09:31:56 +0100

    feat: add route to retrieve user by secu number

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,8 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { DatabaseSingleton } from '../db/database';
 import { users } from '../db/schemas/users';
-import { getAllUsersRoute } from '../openapi/routes/user';
+import { getAllUsersRoute, getUserRoute } from '../openapi/routes/user';
+import { eq } from 'drizzle-orm';
 
 const db = DatabaseSingleton.getInstance().db;
 const userApp = new OpenAPIHono();
@@ -16,6 +17,25 @@ userApp.openapi(getAllUsersRoute, async (c) => {
         .from(users);
 
     return c.json({ users: result }, 200);
+});
+
+userApp.openapi(getUserRoute, async (c) => {
+    const { securityNumber } = c.req.valid('param');
+    const result = await db
+        .select({
+            securityNumber: users.securityNumber,
+            firstName: users.firstName,
+            lastName: users.lastName,
+        })
+        .from(users)
+        .where(eq(users.securityNumber, securityNumber));
+
+    const user = result.shift();
+    if (!user) {
+        return c.text('User not found', 404);
+    }
+
+    return c.json({ user: user }, 200);
 });
 
 export default userApp;
